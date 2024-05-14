f01117b3413f104f2e3929f2a5fb59fea2de559e
Sloane Tribble <sloane.tribble@liatrio.com>
2024-05-14 16:12:22 -0700

    feat: Update host config

---
+++ main.tf
@@ -0,0 +1,13 @@
+ terraform {
+   required_providers {
+     devops-bootcamp = {
+       source = "liatr.io/terraform/devops-bootcamp"
+     }
+   }
+ }
+
+provider "devops-bootcamp" {
+    host = "http://localhost:8080"
+}
+
+
